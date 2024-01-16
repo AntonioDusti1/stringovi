36b6435b01e8956e82641f94a7ec01813feeda1d
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-16 12:42:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_vježba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3 = s1.Substring(0, 8) + s2.Substring(6);
+            Console.WriteLine(s3);
+            Console.ReadLine();
+        }
+    }
+}
