85180aa6a121064899f16ad50507de15bde4df30
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-16 13:05:24 +0100

    Add files via upload

---
+++ 5 zadatak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi cijeli broji i decimalni broji:");
+            string input = Console.ReadLine();
+
+            string[] broj = input.Split(' ');
+
+            int x = int.Parse(broj[0]);
+            double y = double.Parse(broj[1]);
+
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+            Console.ReadLine();
+        }
+    }
+}
