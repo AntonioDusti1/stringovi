615b72c510edb06dde3649c9b06cbb4278d9781d
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-16 12:43:41 +0100

    Add files via upload

---
+++ pomjena slova.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ispis_slova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi rečenicu");
+            string input = Console.ReadLine();
+            string sVeliki = input.ToUpper();
+            Console.WriteLine("sVeliki:" + sVeliki);
+            string sMali = input.ToLower();
+            Console.WriteLine("sMali:" + sMali);
+            string sPrvatri = input.Substring(0, 3);
+            Console.WriteLine("sPrvaTri:" + sPrvatri);
+            string sZadnjiPet = input.Substring(input.Length - 5);
+            Console.WriteLine("sZadnjiPet:" + sZadnjiPet);
+            string s8_11 = input.Substring(7,4 );
+            Console.WriteLine("s8_11:" + s8_11);
+            Console.ReadLine();
+        }
+    }
+}
