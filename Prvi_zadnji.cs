599603b84c8b55b88f27b04a977422f0a4695e12
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-16 12:59:37 +0100

    Add files via upload

---
+++ Prvi_zadnji.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prva_zadnja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi riječ");
+            string riječ= Console.ReadLine();
+            string[] recenica = riječ.Split(" ");
+            string Prvarijec = recenica[0];
+            string zadnjarijec = recenica[recenica.Length - 1];
+
+            Console.WriteLine("Prva riječ: " + Prvarijec);
+            Console.WriteLine("Zadnja riječ: " + zadnjarijec);
+        }
+    }
+}
