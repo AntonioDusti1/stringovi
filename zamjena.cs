2b9f896761a73fe058e728e359119053db226d74
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-16 12:43:22 +0100

    Add files via upload

---
+++ zamjena.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razmak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi recenicu");
+            string input = Console.ReadLine();
+            string razmakString = input.Replace(" ","_");
+            Console.WriteLine("Zamjena recenicu:" + razmakString);
+            Console.ReadLine();
+        }
+    }
+}
